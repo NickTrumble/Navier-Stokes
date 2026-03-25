8d292110300be0aa8c08c38dff6fb7f9be30a2da
Nicholas Trumble <nicktrumble01@gmail.com>
2026-03-25 18:29:19 +0000

    cleaned up UI code, and allowed for changing modes

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ visc = 0.1
 dye = create_dye(SIZE)
 timestep = get_timestep(u, v, visc)
 
+#dye or vectors or fluids
+mode = "dye" 
+
 def add_force(u, v, x, y, radius = 3, strength = 0.5):
     Y, X = np.meshgrid(np.arange(SIZE), np.arange(SIZE), indexing='ij')
     dist = np.sqrt((X - x) ** 2 + (Y- y) ** 2)
@@ -41,12 +44,40 @@ def add_dye(dye, x, y, radius = 2, amount = 0.1):
 
     dye[mask] += amount * falloff
 
+def on_key(event):
+    global mode
+    if event.key == '1':
+        mode = "dye"
+    if event.key == '2':
+        mode = "vectors"
+    if event.key == '3':
+        mode = "fluids"
+
 fig, ax = plt.subplots()
-# (X, Y) = np.meshgrid(np.arange(SIZE), np.arange(SIZE))
-# quiver = ax.quiver(X, Y, u, v)
+(X, Y) = np.meshgrid(np.arange(SIZE), np.arange(SIZE))
+quiver = ax.quiver(X, Y, u, v)
 speed = np.sqrt(u[1:-1, 1:-1] ** 2 + v[1:-1, 1:-1] ** 2)
 im = ax.imshow(dye, cmap='Greys')
 plt.colorbar(im)
+
+def update_plot(i):
+    global mode
+    if mode == "dye":
+        quiver.set_visible(False)
+        ax.set_title(f'Dye, step: {i}')
+        im.set_data(dye)
+        im.set_visible(True)
+    elif mode == "vectors":
+        im.set_visible(False)
+        quiver.set_UVC(u, v)
+        ax.set_title(f'Velocity field, step: {i}')
+        quiver.set_visible(True)
+    else:
+        quiver.set_visible(False)
+        im.set_visible(True)
+        speed = np.sqrt(u[1:-1, 1:-1] ** 2 + v[1:-1, 1:-1] ** 2)
+        im.set_data(speed)
+        ax.set_title(f'Velocity heat map, step: {i}')
 
 def sim():
     global u, v, p, dye
@@ -54,21 +85,18 @@ def sim():
 
     for i in range(1000):
         (u, v, p) = start_simulation(u, v, p)
-        speed = np.sqrt(u[1:-1, 1:-1] ** 2 + v[1:-1, 1:-1] ** 2)
-        
         dye = sim_dye(dye, u, v, timestep, visc)
-        ax.set_title(f'Step: {i}')
-        im.set_data(dye)
-        # quiver.set_UVC(u, v)
-        # ax.set_title(f'Velocity field, step: {i}')
+        update_plot(i)
 
         plt.pause(0.01)
+
     plt.ioff()
     plt.show()
 
 
 
 fig.canvas.mpl_connect("motion_notify_event", on_move)
+fig.canvas.mpl_connect("key_press_event", on_key)
 sim()
 #quiver_plot()
 #imshow_plot()
